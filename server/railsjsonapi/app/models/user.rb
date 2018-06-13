f4e4c9c31d08cabc49df7485fdead9b55934e5c5
hjnp <hp_ctt@hotmail.com>
2018-06-13 11:13:22 +0100

    add note

--- server/railsjsonapi/app/models/user.rb
+++ server/railsjsonapi/app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :confirmable is used for email confirmation setup
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
